4661255389974e4746cf0dec08fc84051110eb88
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-11-10 02:01:24 -0500

    implement Cake SIZE components and style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,39 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            Aurora Cake desing brings the best cake with affordable price!
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.text}>
+            131 Calvington Ave
+            <br /> Kanata, K2T 0L4
+            <br /> (613) 769-2447
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
